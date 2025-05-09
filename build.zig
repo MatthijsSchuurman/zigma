4610557043678f42de87b63c03762594676663bf
Matthijs Schuurman <git@matthijsschuurman.nl>
2025-05-09 17:13:17 +0200

    build test all

--- build.zig
+++ build.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) void {
+pub fn build(b: *std.Build) !void {
   const target = b.standardTargetOptions(.{});
   const optimize = b.standardOptimizeOption(.{});
 
@@ -15,7 +15,7 @@ pub fn build(b: *std.Build) void {
   // Setup exe
   const exe = b.addExecutable(.{
     .name = demo_name orelse "default",
-    .root_source_file = .{ .cwd_relative = std.fmt.allocPrint(b.allocator, "{s}/main.zig", .{demo_name orelse "default"}) catch unreachable },
+    .root_source_file = .{ .cwd_relative = try std.fmt.allocPrint(b.allocator, "{s}/main.zig", .{demo_name orelse "default"})},
     .target = target,
     .optimize = optimize,
   });
@@ -35,18 +35,32 @@ pub fn build(b: *std.Build) void {
   b.default_step.dependOn(&exe.step);
 
   // Define test command
+  var argv = std.ArrayList([]const u8).init(b.allocator);
+  try argv.append("zig");
+  try argv.append("test");
+  //try argv.append("-femit-llvm-ir");
+
+  inline for (system_libs) |lib| {
+    try argv.append("-l" ++ lib);
+  }
+
+  const test_step = b.step("test", "Run unit tests");
   if (test_file) |path| {
-    var argv = std.ArrayList([]const u8).init(b.allocator);
-    argv.append("zig") catch unreachable;
-    argv.append("test") catch unreachable;
-    argv.append(path) catch unreachable;
-    //argv.append("-femit-llvm-ir") catch unreachable;
+    try argv.append(path);
+    test_step.dependOn(&b.addSystemCommand(argv.items).step);
+    _ = argv.pop();
+  } else {
+    var dir = try std.fs.cwd().openDir("./", .{.iterate = true});
+    defer dir.close();
 
-    inline for (system_libs) |lib| {
-      argv.append("-l" ++ lib) catch unreachable;
+    var it = try dir.walk(b.allocator);
+    while (try it.next() ) |entry| {
+      if (entry.kind != .file) continue;
+      if (!std.mem.endsWith(u8, entry.path, "_test.zig")) continue;
+
+      try argv.append(entry.path);
+      test_step.dependOn(&b.addSystemCommand(argv.items).step);
+      _ = argv.pop();
     }
-
-    const run_tests = b.addSystemCommand(argv.items);
-    b.step("test", "Run unit tests").dependOn(&run_tests.step);
   }
 }
