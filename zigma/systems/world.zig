441dfe792ffd52d2425b221efd668bf953e5dbce
Matthijs Schuurman <git@matthijsschuurman.nl>
2025-06-15 14:46:36 +0200

    world system timelines syncing

--- zigma/systems/world.zig
+++ zigma/systems/world.zig
@@ -14,6 +14,8 @@ pub const System = struct {
   }
 
   pub fn update(self: *System) bool {
+    const world_timeline = self.world.components.timeline.get(self.world.entity("timeline").id) orelse return false; // No timeline, prolly testing
+
     var it = self.world.components.world.iterator();
     while(it.next()) |entry| {
       const id = entry.key_ptr.*;
@@ -27,6 +29,10 @@ pub const System = struct {
       defer self.world.allocator.free(active_events);
 
       if (active_events.len > 0) { // World active
+        // Ensure sub world timeline system correctly adjust timeline
+        const subworld_timeline = sub.world.components.timeline.getPtr(sub.world.entity("timeline").id) orelse @panic("World system update no timeline");
+        subworld_timeline.timestampPreviousMS = world_timeline.timestampPreviousMS - @as(i64, @intFromFloat(world_timeline.timeDelta*1000));
+
         return sub.world.render(); // Only render sub world
       }
     }
@@ -45,31 +51,42 @@ test "System should render sub world" {
   var universe = ecs.World.init(tst.allocator);
   defer universe.deinit();
   var world = ecs.World.init(tst.allocator);
+  world.initSystems();
   defer world.deinit();
 
-  var system = System.init(&universe);
-  var system_timeline = System.init(&universe);
+  var universe_system_timeline = SystemTimeline.System.init(&universe);
+  var universe_system = System.init(&universe);
 
-  _ = universe.entity("timeline").timeline();
-  _ = world.entity("timeline").timeline();
+  const universe_timeline_entity = universe.entity("timeline").timeline();
+  const world_timeline_entity = world.entity("timeline").timeline();
 
-  const entity = universe.entity("world").subWorld(&world)
+  _ = universe.entity("world").subWorld(&world)
   .event(.{.start = 0, .end= 60});
 
+  const timestampPreviousMS = std.time.milliTimestamp();
+  const universe_timeline = universe.components.timeline.getPtr(universe_timeline_entity.id) orelse unreachable;
+  universe_timeline.timestampPreviousMS = timestampPreviousMS-100;
+
+  _ = universe_timeline_entity.timeline_speed(2);
+  _ = world_timeline_entity.timeline_speed(1.5);
+
   // When
-  _ = universe.entity("timeline").timeline_offset(0.5);
-  _ = system_timeline.update();
-  const result = system.update();
+  _ = universe_system_timeline.update();
+  const result = universe_system.update();
+
+  rl.BeginDrawing(); //Nasty but it does ensure cleanup... no way around this as sub.world.render() has to be called for this test
+  rl.ClearBackground(rl.BLACK);
+  rl.EndDrawing();
 
   // Then
   try tst.expectEqual(true, result);
-  if (world.components.timeline.get(entity.id)) |timeline| {
+  if (world.components.timeline.get(world_timeline_entity.id)) |timeline| {
     try tst.expectEqual(ecs.Components.Timeline.Component{
-      .speed = 1.0,
-      .timeCurrent = 2.0,
+      .speed = 1.5,
+      .timeCurrent = 0.3,
       .timePrevious = 0.0,
-      .timeDelta = 3.0,
-      .timestampPreviousMS = 5.0,
+      .timeDelta = 0.3,
+      .timestampPreviousMS = timestampPreviousMS,
     }, timeline);
   } else
     return error.TestExpectedTimeline;
